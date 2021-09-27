1a4bb04a1650128b8794cf15cbfb55e8ddc5fd28
Adrián E. Córdoba <software.asia@gmail.com>
2021-09-26 21:53:29 -0300

    Final structure.

--- coopderqui.com.ar/js/script.js
+++ coopderqui.com.ar/js/script.js
@@ -1,3 +1,18 @@
+window.onscroll = function() { fixMenu() };
+
+var menuBar = document.getElementById("menu-bar");
+var topPosition = menuBar.offsetTop;
+
+
+function fixMenu() {
+    if (window.pageYOffset >= topPosition && window.innerWidth < 992) {
+        menuBar.classList.add("fixed-menu-bar")
+    } else {
+        menuBar.classList.remove("fixed-menu-bar");
+    }
+}
+
+
 function openMenu() {
     document.getElementById("menu").style.height = "50%";
 }
